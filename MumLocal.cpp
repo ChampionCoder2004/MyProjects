5ac6ef37e148c14c439f0ad572d6f394b686ef83
Parth Satvekar <140697066+ParthByteBard@users.noreply.github.com>
2024-01-27 13:28:04 +0530

    Create MumLocal.cpp

---
+++ MumLocal.cpp
@@ -0,0 +1,10 @@
+  cout << "Station code doesnot exist !" << endl;
+            for(int i=0;i<10;i++)
+            {
+                cout << ".";
+                Sleep(250);
+            }
+            system("CLS");
+            cout << "\n\n\n\n\n\t\t\t\t\tPlease re-enter station code !" << endl;
+            Sleep(2000);
+            userinput(map);
